7b56d6d7d41dfa591c7923c8936e8c8e06a7dfbf
shamsAlamNaimi <naimi.s@husky.neu.edu>
2021-02-10 23:49:51 -0500

    adding react part

---
+++ react client/src/pages/songBj/BillyJoe.js
@@ -0,0 +1,30 @@
+import React from 'react'
+import Paper from '@material-ui/core/Paper'
+import ReactMarkdown from 'react-markdown'
+import 'github-markdown-css'
+import  { useState, useEffect } from 'react';
+
+const BillyJoe = () => {
+    const [markdown, setmarkdown] = React.useState("no data found");
+    useEffect(() => {
+        fetch("https://localhost:5001/BillyJoel")
+        .then(function(response) {
+          return response.json();
+        })
+        .then(res=> setmarkdown(res.data))
+        .catch((err)=>console.log("error"));
+      }, [])
+
+  return (
+    <Paper>
+        <div style={{ backgroundColor: 'white', padding: 90 }}>
+        <ReactMarkdown
+            className="markdown-body"
+            source={markdown}
+            escapeHtml={false}
+          />
+        </div>
+    </Paper>
+  )
+}
+export default BillyJoe
